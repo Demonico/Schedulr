0060ec4d2a7ba1d42d736bc8317577675a2219b1
Jarrod <Demonico@users.noreply.github.com>
2018-12-14 07:27:29 -0700

    changed files to tsx extension

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <h1>Hello Schedulr</h1>
         </header>
       </div>
     );
